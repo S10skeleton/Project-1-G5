4be6cdbb57c61f11199d32f28f1d3b38728c56f1
Shane <s10skeleton@gmail.com>
2023-12-03 22:52:07 -0700

    background JS and css updated

--- assets/script.js
+++ assets/script.js
@@ -216,4 +216,37 @@ function userNotFound() {
   var gitRepoLists = document.getElementById('gitRepoLists');
   gitRepoLists.innerHTML = '<p>User not found</p>';
 }
+
+
+function updateBackgroundImage() {
+  const now = new Date();
+  const hour = now.getHours();
+  let backgroundImage = '';
+
+  if (hour >= 0 && hour < 3) {
+      backgroundImage = 'url(assets/Images/nighttime.png)';
+  } else if (hour >= 3 && hour < 6) {
+      backgroundImage = 'url(assets/Images/nighttime.png)';
+  } else if (hour >= 6 && hour < 9) {
+      backgroundImage = 'url(assets/Images/illustration sunrise.png)';
+  } else if (hour >= 9 && hour < 12) {
+      backgroundImage = 'url(assets/Images/lateMorning.png)';
+  } else if (hour >= 12 && hour < 15) {
+      backgroundImage = 'url(assets/Images/midDay.png)';
+  } else if (hour >= 15 && hour < 18) {
+      backgroundImage = 'url(assets/Images/afternoon.png)';
+  } else if (hour >= 18 && hour < 21) {
+      backgroundImage = 'url(assets/Images/nighttime.png)';
+  } else {
+      backgroundImage = 'url(assets/Images/nighttime.png)';
+  }
+
+  document.body.style.backgroundImage = backgroundImage;
+}
+
+// Call the function to set the initial background
+updateBackgroundImage();
+
+setInterval(updateBackgroundImage, 3600000);
+
 //end of GitHub section --------------------------------------------------------------
